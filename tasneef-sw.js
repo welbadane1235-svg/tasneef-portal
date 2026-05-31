4e207889720995fcb9598ba3035c3a17e66a514c
Hier x <wel.badane1235@gmail.com>
2026-05-31 14:11:45 +0300

    Add files via upload

--- tasneef-sw.js
+++ tasneef-sw.js
@@ -1,17 +1,2 @@
-/* TASNEEF V275 cache reset service worker: removes old cached builds and uses network only. */
-self.addEventListener('install', event => {
-  event.waitUntil((async()=>{
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k=>/tasneef/i.test(k)).map(k=>caches.delete(k)));
-    await self.skipWaiting();
-  })());
-});
-self.addEventListener('activate', event => {
-  event.waitUntil((async()=>{
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k=>/tasneef/i.test(k)).map(k=>caches.delete(k)));
-    try{ await self.registration.unregister(); }catch(e){}
-    await self.clients.claim();
-  })());
-});
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
+self.addEventListener('install', e=>self.skipWaiting());
+self.addEventListener('activate', e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
